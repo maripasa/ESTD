07e894b29b26be456015b608a2ebfafa4a4220da
maripasa <asafexi@proton.me>
2024-12-01 11:12:00 -0300

    finished this thing

--- lists/hash_project.py
+++ lists/hash_project.py
@@ -1,4 +1,32 @@
-from functools import reduce
 from itertools import product
+from functools import reduce
 
-print(*{k: v for k, v in sorted({list(product(["sum", "polynomial", "shift"], ["division", "fold", "mad"]))[i]: result for i, result in enumerate([32 - len({f2(f1(key)) for key in ["apple", "voadora", "banjo", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "xuru", "runin", "xamã", "mirtilho", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry", "strawberry", "tangerine", "ugli", "voavanga", "maravilha", "IFCE", "maracanaú", "ceará", "manga", "rendemption", "bobo", "maluco"]}) for f1, f2 in product([lambda x: sum(ord(c) for c in x), lambda x: sum(ord(c) * 31 ** i for i, c in enumerate(x)), lambda x: reduce(lambda h, c: ((h << 1 & (1 << 32) - 1) | (h >> 31)) + ord(c), x, 0), ], [lambda x: x % 32, lambda x: (lambda r: sum(int(digit) for digit in str(r)) if r > 31 else r)(reduce(lambda p, q: p + q, list(map(lambda z: int(z), [str(x)[i:i+2] for i in range(0, len(str(x)), 2)])))), lambda x: ((17 * x + 11) % 83) % 31,])])}.items(), key=lambda item: item[1], reverse=True)}.items(), sep="\n")
+def fold(x):
+    num = str(x)
+    if len(num) <= 2:
+        return x % 32
+    if len(num) % 2 != 0:
+        num = "0" + num
+    num = str((int(num[1]) + int(num[2])) % 10) + str((int(num[0]) + int(num[3])) % 10) + num[4:]
+    return fold(int(num))
+
+keys = ["apple", "voadora", "banjo", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "xuru", "runin", "xamã", "mirtilho", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry", "strawberry", "tangerine", "ugli", "voavanga", "maravilha", "IFCE", "maracanaú", "ceará", "manga", "rendemption", "bobo", "maluco"]
+
+hash_functions = [
+    ('sum', lambda x: sum(ord(c) for c in x)),
+    ('polynomial', lambda x: sum(ord(c) * (31 ** i) for i, c in enumerate(reversed(x)))),
+    ('shift', lambda x: reduce(lambda h, c: ((h << 1) & 0xFFFFFFFF) | (h >> 31) + ord(c), x, 0))
+]
+
+compression_functions = [
+    ('division', lambda x: x % 32),
+    ('fold', fold),
+    ('mad', lambda x: (17 * x + 11) % 83 % 32)
+]
+
+results = {}
+for (h_name, h_func), (c_name, c_func) in product(hash_functions, compression_functions):
+    results[(h_name, c_name)] = len(keys) - len({c_func(h_func(key)) for key in keys})
+
+for (h, c), v in sorted(results.items(), key=lambda x: x[1], reverse=True):
+    print(f"{h}, {c}: {v}")
